f6d5c54c46c67fd35abac198931f2506ec6298d3
Sercan Yılmaz <52522145+sercanyilmaz84@users.noreply.github.com>
2020-06-08 18:57:33 +0300

    Update webfox.py

--- webfox.py
+++ webfox.py
@@ -16,22 +16,23 @@ a = """
 """
 print(a)
 print("*****************************************************")
-print("Version: 1\nContact: bilgi@sercanyilmaz.com.tr")
+print("Version: 1.2\nContact: bilgi@sercanyilmaz.com.tr")
 print("*****************************************************")
 time.sleep(3)
 
 def user_input():
     parse_object = optparse.OptionParser()
-    parse_object.add_option("-i", "--ip", dest="ip_address", help= "Enter target IP")
+    parse_object.add_option("-t", "--target", dest="target_address", help= "Enter domain")
     options = parse_object.parse_args()[0]
     
-    if not options.ip_address:
+    if not options.target_address:
         print("-i, you must enter your target IP")
     
     return options
+    
 
 host_info = user_input()
-ip = host_info.ip_address
+target = host_info.target_address
 
 # ColorsPalet
 OKGREEN = '\033[92m'
@@ -39,18 +40,16 @@ ENDC = '\033[0m'
 BOLD = '\033[1m'
 WARNING = '\033[93m'
 
-urlfile = open("YOUR_FILE_PATH\common_urllist.txt")
+urlfile = open("YOUR_FILE_PATH\common_urllist.txt") #CHANGE_THIS
 u = urlfile.read()
 hiddenurls = u.splitlines()
 
-subfile = open("YOUR_FILE_PATH\subdomains.txt")
-su = subfile.read()
-subdomains = su.splitlines()
 
-def find_hiddenurls(ip):
+
+def find_hiddenurls(target):
     print(WARNING + "\n[!] Hidden Directory Scanner Started!" + ENDC)
     for hiddenurl in hiddenurls:
-        h_url = f"http://{ip}/{hiddenurl}"
+        h_url = f"http://{target}/{hiddenurl}"
         try:
             rr = requests.get(h_url)
 
@@ -59,40 +58,43 @@ def find_hiddenurls(ip):
         else:
             if str(rr.status_code) == "200":
                 print(OKGREEN + "\n[+] Discovered url:", h_url + ENDC)
-                print (OKGREEN + h_url + "'s status code => "+ str(rr.status_code) + ENDC)
 
-def find_subdomains(ip):
+
+subfile = open("YOUR_FILE_PATH\subdomains.txt") #CHANGE_THIS
+su = subfile.read()
+subdomains = su.splitlines()
+
+def find_subdomains(target):
     print(WARNING + "\n[!] Subdomain Scanner Started!" + ENDC)
     for subdomain in subdomains:
-        url = f"http://{subdomain}.{ip}"
+        url = f"http://{subdomain}.{target}"
         try:
-            r = requests.get(url)
+            requests.get(url)
         except requests.ConnectionError:
             pass
         else:
             print(OKGREEN + "\n[+] Discovered subdomain:", url + ENDC)
-            print (OKGREEN + url + "'s status code => "+ str(r.status_code) + ENDC)
             
 
-def TCP_connect(ip, port_number, delay, output):
+def TCP_connect(target, port_number, delay, output):
     TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     TCPsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     TCPsock.settimeout(delay)
     try:
-        TCPsock.connect((ip, port_number))
+        TCPsock.connect((target, port_number))
         output[port_number] = 'is open'
     except:
         output[port_number] = ''
 
 
-def scan_ports(ip, delay):
+def scan_ports(target, delay):
     print(WARNING + "\n[!] Port Scanner Started!\n" + ENDC)
 
     threads = []
     output = {}
 
     for i in range(10000):
-        t = threading.Thread(target=TCP_connect, args=(ip, i, delay, output))
+        t = threading.Thread(target=TCP_connect, args=(target, i, delay, output))
         threads.append(t)
 
     for i in range(10000):
@@ -105,13 +107,15 @@ def scan_ports(ip, delay):
         if output[i] == 'is open':
             print(OKGREEN + "[+] Port "+ str(i) + " is open" + ENDC)
 
+
+
 def main():
-    host_ip = ip
+    host_ip = target
     delay = int(5)   
     scan_ports(host_ip, delay)
 
 if __name__ == "__main__":
     main()
 
-find_subdomains(ip)
-find_hiddenurls(ip)
+find_hiddenurls(target)
+find_subdomains(target)
